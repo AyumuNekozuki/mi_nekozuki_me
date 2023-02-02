fee8b91ab0fdcf087fc1b1fb5303b12c28690b5b
AyumuNekozuki <ayumu@nekozuki.me>
2023-02-02 12:18:27 +0900

    fix meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,15 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <></>;
+	return (
+		<>
+			<Head>
+				<title>みすきーねこづき</title>
+				<meta name='description' content='みすきーねこづき' />
+				<link rel='icon' href='https://misskey-white.s3.ap-northeast-1.amazonaws.com/mashiro/mashiro_bg_512.png' />
+				<link rel='apple-touch-icon' ref='https://misskey-white.s3.ap-northeast-1.amazonaws.com/mashiro/mashiro_bg_512.png' />
+			</Head>
+		</>
+	);
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,12 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
 	return (
 		<Html lang='ja'>
-			<Head>
-				<title>みすきーねこづき</title>
-				<meta name='description' content='みすきーねこづき' />
-				<link rel='icon' href='https://misskey-white.s3.ap-northeast-1.amazonaws.com/mashiro/mashiro_bg_512.png' />
-				<link rel='apple-touch-icon' ref='https://misskey-white.s3.ap-northeast-1.amazonaws.com/mashiro/mashiro_bg_512.png' />
-			</Head>
+			<Head />
 			<body>
 				<Main />
 				<NextScript />

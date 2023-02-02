6b3f0c5e813e5d9c848dc76ba9758e71d7f0b603
AyumuNekozuki <ayumu@nekozuki.me>
2023-02-02 12:12:07 +0900

    add meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
 	return (
 		<Html lang='ja'>
-			<Head />
+			<Head>
+				<title>みすきーねこづき</title>
+				<meta name='description' content='みすきーねこづき' />
+				<link rel='icon' href='https://misskey-white.s3.ap-northeast-1.amazonaws.com/mashiro/mashiro_bg_512.png' />
+				<link rel='apple-touch-icon' ref='https://misskey-white.s3.ap-northeast-1.amazonaws.com/mashiro/mashiro_bg_512.png' />
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
